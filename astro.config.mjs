7e297b08ec4bd4aaa9af20ee265cc67a8468e20c
yuhuibio <felecia.hamilton01@gmail.com>
2026-06-22 21:07:23 +0800

    移除base路径，适配Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,3 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  base: "/yuhui-products/"
-});
+export default defineConfig({});
