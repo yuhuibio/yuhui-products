3cfd0e9633fabceedc2d2c75bb27494ca70da4c6
yuhuibio <felecia.hamilton01@gmail.com>
2026-06-23 20:21:00 +0800

    Fix build configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://yuhui-products.pages.dev/', // 必须加上这一行
+  base: '/',                                // 必须加上这一行
   integrations: [tailwind()]
 });
