0b262c24abad3a5c5d746b19356df14958dbfc81
yuhuibio <felecia.hamilton01@gmail.com>
2026-06-26 17:14:20 +0800

    redirects update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,5 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://yuhuibiotec.com',
   base: '/',
-  integrations: [tailwind(), sitemap()],
-  // 在这里添加重定向规则
-  redirects: {
-    '/grounding-api-redirect/[...path]': '/blog',
-  }
+  integrations: [tailwind(), sitemap()]
 });
