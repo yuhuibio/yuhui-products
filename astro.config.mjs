9d61fb79c22eddc866a8b5046099c5afd57eaf04
yuhuibio <felecia.hamilton01@gmail.com>
2026-08-18 16:41:53 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://yuhuibiotec.com',
+  site: 'https://www.yuhuibiotec.com', 
   base: '/',
   trailingSlash: 'ignore',
   integrations: [
